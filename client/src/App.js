fb64a4789c0a3ade829e3a9d92fe69b6a91499e9
Wilson Chan <wilsonchan54@gmail.com>
2023-01-22 21:56:42 -0500

    Connected theme.js, redux state, and dashboard route in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import createTheme from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+
+import { themeSettings } from "theme";    // theme.js
 
 function App() {
-  return (
-    <div className="app">
 
-    </div>
+  const mode = useSelector((state) => state.global.mode);     // grabs state in index.js
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])     // mode is passed in theme.js
+
+  return (<div className="app">
+    <BrowserRouter>  
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Navigate to='/dashboard' replace/>} />
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  </div>
   );
 }
+
+// CssBaseline reset styling
 
 export default App;
