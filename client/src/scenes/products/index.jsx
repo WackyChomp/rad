80da1837868f5d74f843b5883c28b90d8617a3f1
Wilson Chan <wilsonchan54@gmail.com>
2023-04-05 19:00:33 -0400

    Created Product component with mui card component in client scene products page

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -3,6 +3,70 @@ import { Box, Card, CardActions, CardContent, Collapse, Button, Typography, Rati
 import { useGetProductsQuery } from 'state/api';
 import Header from 'components/Header';
 
+const Product = ({
+  _id,
+  name,
+  description,
+  price, 
+  rating,
+  category,
+  supply,
+  stat
+}) => {
+  const theme = useTheme();
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return(
+    <Card
+      sx={{
+        backgroundImage: 'none',
+        backgroundColor: theme.palette.background.alt,      // avoid hard coding colors
+        borderRadius: '0.55rem'
+      }}
+    >
+      <CardContent>
+        <Typography sx={{ fontSzie: 14 }} color={theme.palette.secondary[700]} gutterBottom>
+          {category}
+        </Typography>
+        <Typography variant='h5' component='div'>
+          {name}
+        </Typography>
+        <Typography sx={{ mb: '1.5rem' }} color={theme.palette.secondary[400]}>
+          ${Number(price).toFixed(2)}
+        </Typography>
+        <Rating value={rating} readOnly />
+
+        <Typography variant='body2'>{description}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant='primary'
+          size='small'
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          See More Products
+        </Button>
+      </CardActions>
+      <Collapse
+        in={isExpanded}
+        timeout='auto'
+        unmountOnExit
+        sx={{
+          color: theme.palette.neutral[300]
+        }}
+      >
+          <CardContent>
+            <Typography>id: {_id}</Typography>
+            <Typography>Supply Left: {supply}</Typography>
+            <Typography>Yearly Sales This Year: {stat.yearlySalesTotal}</Typography>
+            <Typography>Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}</Typography>
+          </CardContent>
+      </Collapse>
+    </Card>
+  )
+}
+
+
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
   console.log('data', data);
