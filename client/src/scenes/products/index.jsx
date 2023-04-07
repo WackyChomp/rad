7b30650f995792c073405982096c08cea18de894
Wilson Chan <wilsonchan54@gmail.com>
2023-04-06 20:31:59 -0400

    Created Products component to reference data from Product component / styled Box component to display each card (not working)

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -75,6 +75,43 @@ const Products = () => {
   return (
     <Box>
       <Header title='PRODUCTS' subtitle='List of products' />
+      { data || !isLoading ?(
+        <Box 
+          mt='20px' 
+          display='grid' 
+          gridTemplateColumns='repeat(4, minmax(0, 1fr)'
+          justifyContent='space-between'
+          rowGap='20px'
+          columnGap='1.7%'
+        >
+          {data.map(
+            ({
+              _id,
+              name,
+              description,
+              price, 
+              rating,
+              category,
+              supply,
+              stat
+            }) => (
+              <Product
+                key={_id}
+                _id={_id}
+                name={name}
+                description={description}
+                price={price}
+                rating={rating}
+                category={category}
+                supply={supply}
+                stat={stat}
+              />
+            )
+          )}
+        </Box> 
+      ) : (
+        <>Loading...</>
+      )}
     </Box>
   )
 }
