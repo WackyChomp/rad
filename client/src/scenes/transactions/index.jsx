49c2d3708e408e720da485ee6c6f2b51dd28e3e7
Wilson Chan <wilsonchan54@gmail.com>
2023-04-15 09:25:22 -0400

    Created table with mui DataGrid to display transaction data from server / styled Box and DataGrid components (both used customers page as reference)

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -23,8 +23,77 @@ const Transactions = () => {
 
 
 
+  const columns = [
+    {
+      field:'_id',
+      headerName:'ID',
+      flex:1,
+    },
+    {
+      field:'userId',
+      headerName:'User Id',
+      flex:1,
+    },
+    {
+      field:'createdAt',
+      headerName:'Created At',
+      flex:1,
+    },
+    {
+      field:'products',
+      headerName:'# of Products',
+      flex:0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length
+    },
+    {
+      field:'cost',
+      headerName:'Cost',
+      flex:1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`
+    },
+  ]
 
-  return( <div>Transactions O_^ ---- 8^)</div>
+
+  return( 
+  <div>Transactions O_^ ---- 8^)</div>,
+  <Box m='1.5rem 2.5rem'>
+    <Header title='TRANSACTIONS' subtitle='Entire List of Transactions' />
+    <Box 
+      height='80vh'
+      sx={{
+        '& .MuiDataGrid-root': {
+          border: 'none'
+        },
+        '& .MuiDataGrid-cell': {
+          borderBottom: 'none'
+        },
+        '& .MuiDataGrid-columnHeaders': {
+          backgroundColor: theme.palette.background.alt,
+          color: theme.palette.secondary[100],
+          borderBottom: 'none'
+        },
+        '& .MuiDataGrid-virtualScroller': {
+          backgroundColor: theme.palette.primary.light,
+        },
+        '& .MuiDataGrid-footerContainer': {
+          backgroundColor: theme.palette.background.alt,
+          color: theme.palette.secondary[100],
+          borderTop: 'none'
+        },
+        '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+          color: `${theme.palette.secondary[200]} !important`
+        }
+      }}
+    >
+      <DataGrid 
+        loading={ isLoading || !data }
+        getRowId={(row) => row._id}
+        rows={ (data && data.transactions) || [] }
+        columns={ columns }
+      />
+    </Box>
+  </Box>
   )
 }
 
