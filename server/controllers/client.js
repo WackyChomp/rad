79bc44fc9bdfa1f2c12a640c552533448ffcc9f6
Wilson Chan <wilsonchan54@gmail.com>
2023-04-18 17:20:06 -0400

    Added geography route in server / created geography controller in server client.js to format data for geographical visualization

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -3,6 +3,8 @@ import ProductStat from '../models/ProductStat.js';
 import User from '../models/User.js';
 import Transaction from '../models/Transaction.js';
 
+import getCountryISO3 from 'country-iso-2-to-3';
+
 // Products Page
 export const getProducts = async(req, res) => {
 	try{
@@ -78,4 +80,33 @@ export const getTransactions = async (req, res) => {
 	} catch (error) {
 		res.status(404).json({ message: error.message })
 	}
+}
+
+
+// Geography Page
+export const getGeography = async(req, res) => {
+	try{
+		const users = await User.find();
+
+		// convert to countryISO3 format
+		const mappedLocations = users.reduce((acc, {country}) => {			// acc - accumulator
+			const countryISO3 = getCountryISO3(country)
+			if (!acc[countryISO3]){
+				acc[countryISO3] = 0;
+			}
+			acc[countryISO3]++;
+			return acc;
+		}, {});				// add to this object , key = countries , value = number of users
+
+		// Final formatting for NIVO geography chart , turn into key value pair
+		const formattedLocations = Object.entries(mappedLocations).map(
+			([country, count]) => {
+				return { id:country, value:count }
+			}
+		)
+		
+		res.status(200).json(formattedLocations);
+	} catch (error){
+			res.status(404).json({ message: error.message })        // errors should be more specific in complex projects
+		}
 }
