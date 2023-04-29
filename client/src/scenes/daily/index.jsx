5451dff5d1180d27de3796637756b19bcc9b4d7b
Wilson Chan <wilsonchan54@gmail.com>
2023-04-28 23:57:21 -0400

    Modified data format code snippet from OverviewChart component for scene daily page

--- client/src/scenes/daily/index.jsx
+++ client/src/scenes/daily/index.jsx
@@ -7,6 +7,45 @@ import ReactDatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";        // https://github.com/Hacker0x01/react-datepicker
 
 const Daily = () => {
+	const [startDate, setStartDate] = useState(new Date('2023-01-01'));
+	const [endDate, setEndDate] = useState(new Date('2023-04-01'));
+	const { data } = useGetSalesQuery();		// data from useGetSalesQuery
+	const theme = useTheme();
+
+	const [formattedData] = useMemo(() => {
+    if (!data) return [];
+
+    const { dailyData } = data;
+    const totalSalesLine = {
+      id:'totalSales',
+      color: theme.palette.secondary.main,
+      data: [],
+    };
+    const totalUnitsLine = {
+      id:'totalUnits',
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+
+    // Grab values of daily data from server data index.js and formatting it
+    Object.values(dailyData).forEach(({ date, totalSales, totalUnits}) =>{
+      const dateFormatted = new date(date);
+      if(dateFormatted >= startDate && dateFormatted <= endDate){
+        const splitDate = date.substring(date.indexOf('-' + 1));  // grab contents after '-'
+
+        totalSalesLine.data = [
+          ...totalSalesLine.data,
+          { x:splitDate, y:totalSales }
+        ];
+        totalUnitsLine.data = [
+          ...totalUnitsLine.data,
+          { x:splitDate, y:totalUnits }
+        ];
+      }
+    })
+    const formattedData = [totalSalesLine, totalUnitsLine]
+    return [formattedData]
+	}, [data, startDate, endDate])        // eslint-disable-line react-hooks/exhaustive-deps
 
 	return (  
     <div>Daily</div>
