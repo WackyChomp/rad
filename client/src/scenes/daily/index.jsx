923376323cadd245c4d1e636e5bfea03b900b7c6
Wilson Chan <wilsonchan54@gmail.com>
2023-04-29 17:22:16 -0400

    Added react DatePicker component code snippet and modified nivo ResponsiveLine component from OverviewChart component for daily page / added route for daily page in client App.js

--- client/src/scenes/daily/index.jsx
+++ client/src/scenes/daily/index.jsx
@@ -48,7 +48,129 @@ const Daily = () => {
 	}, [data, startDate, endDate])        // eslint-disable-line react-hooks/exhaustive-deps
 
 	return (  
-    <div>Daily</div>
+    <div>Daily Dosages Don't Demand Delights :{'>'}</div>,
+    <Box m='2rem 3rem'>
+      <Header title='DAILY SALES' subtitle='Chart of daily ' />
+      <Box heigh='80vh'>
+        <Box display='flex' justifyContent='flex-end' />
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+        />
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+        />
+
+
+    {/* Modified and reused from OverviewChart component */}
+    {data ? (
+          <ResponsiveLine
+          theme={{
+            axis: {
+              domain: {
+                line: {
+                  stroke: theme.palette.secondary[200]
+                }
+              },
+              legend: {
+                text: {
+                  fill: theme.palette.secondary[200]
+                }
+              },
+              tick: {
+                line: {
+                  stroke: theme.palette.secondary[200],
+                  strokeWidth: 1,
+                },
+                text: {
+                  fill: theme.palette.secondary[200]
+                }
+              }
+            }}}
+            data={formattedData}
+            color={{ datum: 'color' }}    // use color data 
+            margin={{ top: 20, right: 50, bottom: 50, left: 90 }}    // top: 50, right: 110, bottom: 50, left: 60
+            xScale={{ type: 'point' }}
+            yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: false,     // prevent lines from stacking on top of each other
+                reverse: false
+            }}
+            yFormat=" >-.2f"
+            curve='catmullRom'
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                format: (v) => {
+                  if (isDashboard) return v.slice(0,3);     // shows abbreviated months on dashboard page
+                  return v;
+                },
+                orient: 'bottom',
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 90,
+                legend: 'Month',     // 'transportation'
+                legendOffset: 60,
+                legendPosition: 'middle'
+            }}
+            axisLeft={{
+                orient: 'left',
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Total',      // 'count'
+                legendOffset: -50,
+                legendPosition: 'middle'
+            }}
+            pointSize={10}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabelYOffset={-12}
+            useMesh={true}
+            legends={
+              [
+                {
+                    anchor: 'top-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 60,
+                    translateY: 0,
+                    itemsSpacing: 0,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 80,
+                    itemHeight: 20,
+                    itemOpacity: 0.75,
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemBackground: 'rgba(0, 0, 0, .03)',
+                                itemOpacity: 1
+                            }
+                        }
+                    ]
+                }
+              ]
+            }
+          />
+    ) : <>I'm Loadin Ovah Here...</>}
+        
+      </Box>
+    </Box>
   )
 }
 
